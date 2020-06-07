d55804c47fd13f0cff6e2400fbe979d8c7e5cf60
prinzz1208 <priyanshu81212@gmail.com>
2020-06-07 20:01:57 +0530

    phone responsive

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import Movie from './Movie/Movie';
+import './Movies.css';
 const Movies = (props) => {
-    const style = {
-        maxWidth:'50%',
-        height:'25rem',
-        margin:'0 auto',
-        fontSize:'1.5rem',
-        overflowY:'auto',
-        lineHeight:'2rem', 
-        padding: '0'   
-    }
+ 
     const movies = (
         props.movies.map((el) => (
             <Movie key={el.id} movie={el.movie} deleteMovie = {props.delete} id={el.id} />
@@ -17,7 +10,7 @@ const Movies = (props) => {
     )
     return(
         <React.Fragment>
-            <ul className="" style={style}>{movies}</ul>
+            <ul className="Movies">{movies}</ul>
         </React.Fragment>
     )
 }
